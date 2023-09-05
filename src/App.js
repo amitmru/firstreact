23114eef204c963c374155442ed4ff8d8d8ddaa4
amitmru <amitmaru2017@gmail.com>
2023-09-05 16:58:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Componate
+          Learn React Componate (Home Work)
         </a>
       </header>
     </div>
